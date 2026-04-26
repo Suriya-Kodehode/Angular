e21a70282c1734d2d9f45f3524330bb81d15e384
Suriya-Kodehode <suriya.kodehode@gmail.com>
2026-04-26 20:50:25 +0200

    feat(navbar): add adaptive svg hamburger rendering

--- projects/my-app/src/app/shared/components/navbar/navbar.component.ts
+++ projects/my-app/src/app/shared/components/navbar/navbar.component.ts
@@ -1,11 +1,21 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { APP_ICONS } from '../../constants/icons';
+import { AdaptSvgDirective, type AdaptiveSvgIconVm } from '../../directives/adapt-svg.directive';
+import { getAdaptiveSvgColorVar } from '../../utils/adaptive-svg-color.util';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink, AdaptSvgDirective],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  private readonly menuIconDef = APP_ICONS.hamburger;
+
+  protected readonly menuIconVm: AdaptiveSvgIconVm = {
+    src: this.menuIconDef.src,
+    colorVar: getAdaptiveSvgColorVar(this.menuIconDef),
+  };
+}

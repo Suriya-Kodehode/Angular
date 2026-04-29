cf8482d49850ccf239704be092877d869baf4890
Suriya-Kodehode <suriya.kodehode@gmail.com>
2026-04-29 20:56:10 +0200

    feat(shared): export shared barrel members from app shared index

--- projects/my-app/src/app/shared/index.ts
+++ projects/my-app/src/app/shared/index.ts
@@ -7,7 +7,22 @@
 // export { DATE_FORMAT } from './constants/app.constants';
 // export { SpinnerComponent } from './components/spinner/spinner.component';
 
+// Components
 export { ButtonComponent } from './components/button/button.component';
 export type { ButtonVariant, ButtonSize, ButtonType } from './components/button/button.component';
 export { NavbarComponent } from './components/navbar/navbar.component';
 export { SidebarComponent } from './components/sidebar/sidebar.component';
+
+// Directives
+export { AdaptSvgDirective } from './directives/adapt-svg.directive';
+export type { AdaptiveSvgIconVm } from './directives/adapt-svg.directive';
+
+// Models
+export * from './models';
+
+// Constants
+export { APP_ICONS, type AppIconDefinition, type AppIconName } from './constants/icons';
+export { API_PREFIX, API_PREFIX_REGEX } from './constants/regex.constants';
+
+// Utils
+export { getAdaptiveSvgColorVar } from './utils/adaptive-svg-color.util';

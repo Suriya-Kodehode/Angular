16c7255c7594343a6a9f7131928caa3b01af327d
Suriya-Kodehode <suriya.kodehode@gmail.com>
2026-05-16 22:40:04 +0200

    refactor(home): replace raw btn elements with app-button component

--- projects/my-app/src/app/features/home/home.component.ts
+++ projects/my-app/src/app/features/home/home.component.ts
@@ -1,12 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { contextConsole } from '@app/core/services';
+import { ButtonComponent } from '@app/shared';
 
 const console = contextConsole('HomeComponent');
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
+  imports: [RouterLink, ButtonComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
